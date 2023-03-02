60583caeb00f959c64b008db023e0acc4ed8b64c
cale <ne.calero@uniandes.edu.co>
2023-03-01 22:00:25 -0500

    HOTFIX: Cell outline on mobile too thick

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Carcassonne-Challenge',
+  base: '/Carcassonne-Challenge/',
   plugins: [react()],
 });
